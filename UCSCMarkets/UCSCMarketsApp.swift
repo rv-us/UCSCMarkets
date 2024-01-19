fdd3e462dbe9adb2fce9e954c405b0850fa63a34
Rachit <rverma8@ucsc.edu>
2024-01-19 15:25:58 -0800

    Initial Commit

---
+++ UCSCMarkets/UCSCMarketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCSCMarketsApp.swift
+//  UCSCMarkets
+//
+//  Created by Rachit Verma on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct UCSCMarketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
